34ec114b006ec0554b75f4d5e1288af0dc337866
pachikuriii <stakahashi0728@gmail.com>
2022-08-14 22:14:28 +0900

    リストを編集できる状態に

--- main.js
+++ main.js
@@ -1,55 +1,51 @@
-const STORAGE_KEY = 'todos-vuejs-demo'
+const STORAGE_KEY = "todos-vuejs-demo";
 const todoStorage = {
   fetch: function () {
-    const todos = JSON.parse(
-      localStorage.getItem(STORAGE_KEY) || '[]'
-    )
+    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
     todos.forEach(function (todo, index) {
-      todo.id = index
-    })
-    todoStorage.uid = todos.length
-    return todos
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
   },
   save: function (todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
-  }
-}
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  },
+};
 
 const app = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    todos: []
+    todos: [],
   },
   methods: {
-    // ToDo 追加の処理
-    doAdd: function (event, value) {
-      // ref で名前を付けておいた要素を参照
-      const comment = this.$refs.comment
+    doAdd: function (value) {
+      const comment = this.$refs.comment;
       if (!comment.value.length) {
-        return
+        return;
       }
-      // { 新しいID, コメント, 作業状態 }
-      // というオブジェクトを現在の todos リストへ push
-      // 作業状態「state」はデフォルト「作業中=0」で作成
       this.todos.push({
         id: todoStorage.uid++,
         comment: comment.value,
-      })
-      // フォーム要素を空にする
-      comment.value = ''
+        editing: false,
+      });
+      comment.value = "";
     },
-
+    doEdit: function (todo) {
+      todo.editing = true;
+    },
+    doneEdit: function (todo) {
+      todo.editing = false;
+    },
     doRemove: function (item) {
-      const index = this.todos.indexOf(item)
-      const check = confirm('本当に削除しますか？');
+      const index = this.todos.indexOf(item);
+      const check = confirm("本当に削除しますか？");
       if (check === true) {
-        this.todos.splice(index, 1)
+        this.todos.splice(index, 1);
       }
-    }
-
+    },
   },
   created() {
-    // インスタンス作成時に自動的に fetch() する
-    this.todos = todoStorage.fetch()
-  }
-})
+    this.todos = todoStorage.fetch();
+  },
+});
