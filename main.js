62dfedb7e2f133480f969c9264930012244c1840
pachikuriii <stakahashi0728@gmail.com>
2022-08-15 13:58:22 +0900

    コードの整形

--- main.js
+++ main.js
@@ -1,51 +1,56 @@
-const STORAGE_KEY = "todos-vuejs-demo";
+/* global Vue */
+/* eslint-disable no-new */
+
+const STORAGE_KEY = 'vue-todo'
 const todoStorage = {
   fetch: function () {
-    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
     todos.forEach(function (todo, index) {
-      todo.id = index;
-    });
-    todoStorage.uid = todos.length;
-    return todos;
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
   },
   save: function (todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-  },
-};
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
 
-const app = new Vue({
-  el: "#app",
-  data: {
-    todos: [],
+new Vue({
+  el: '#app',
+  data: function () {
+    return {
+      todos: []
+    }
   },
   methods: {
     doAdd: function (value) {
-      const comment = this.$refs.comment;
+      const comment = this.$refs.comment
       if (!comment.value.length) {
-        return;
+        return
       }
       this.todos.push({
         id: todoStorage.uid++,
         comment: comment.value,
-        editing: false,
-      });
-      comment.value = "";
+        editing: false
+      })
+      comment.value = ''
     },
     doEdit: function (todo) {
-      todo.editing = true;
+      todo.editing = true
     },
     doneEdit: function (todo) {
-      todo.editing = false;
+      todo.editing = false
     },
-    doRemove: function (item) {
-      const index = this.todos.indexOf(item);
-      const check = confirm("本当に削除しますか？");
+    doRemove: function (todo) {
+      const index = this.todos.indexOf(todo)
+      const check = confirm('本当に削除しますか？')
       if (check === true) {
-        this.todos.splice(index, 1);
+        this.todos.splice(index, 1)
       }
-    },
+    }
   },
-  created() {
-    this.todos = todoStorage.fetch();
-  },
-});
+  created: function () {
+    this.todos = todoStorage.fetch()
+  }
+})
