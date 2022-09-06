ff4efb514ee34eb394bc8ba49fa872252f3fb260
pachikuriii <stakahashi0728@gmail.com>
2022-09-06 13:03:27 +0900

    編集後のレコードが自動保存されないように

--- main.js
+++ main.js
@@ -16,25 +16,30 @@ const app = {
     return {
       todos: [],
       editedTodo: '',
-      content: ''
+      newTodoContent: '',
+      editedTodoContent: ''
     }
   },
   methods: {
     add: function () {
-      if (!this.content.length) {
+      if (!this.newTodoContent.length) {
         return
       }
       this.todos.push({
         id: new Date().getTime(),
-        content: this.content
+        content: this.newTodoContent
       })
       todoStorage.save(this.todos)
-      this.content = ''
+      this.newTodoContent = ''
     },
     edit: function (targetTodo) {
+      this.editedContent = targetTodo.content
       this.editedTodo = targetTodo
     },
-    doneEdit: function () {
+    doneEdit: function (targetTodo) {
+      const todo = this.todos.find((todo) => todo.id === targetTodo.id)
+      const index = this.todos.indexOf(todo)
+      this.todos[index].content = this.editedContent
       todoStorage.save(this.todos)
       this.editedTodo = ''
     },
